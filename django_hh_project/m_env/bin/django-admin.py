dd9284aa78a72574e3af9a5258b050a7709533c0
mlg1996 <32440812+mlg1996@users.noreply.github.com>
2019-08-08 11:51:52 -0500

    Testing gitignore

---
+++ django_hh_project/m_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/madisongwynn/Desktop/Summer 2019/Advanced Programming/DjangoTest/python-docs-samples/appengine/standard_python37/django/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
