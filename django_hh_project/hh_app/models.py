e056f41538ccedd9e91d266f3bb4f6cfd24eb85e
Brian <beadsdd@gmail.com>
2019-08-08 12:36:06 -0500

    Fixed bug in add bar

--- django_hh_project/hh_app/models.py
+++ django_hh_project/hh_app/models.py
@@ -13,6 +13,8 @@ class Region(models.Model):
 class Feature(models.Model):
 	feature_title = models.CharField(max_length = 100)
 	description = models.CharField(max_length = 100)
+	def __str__(self):
+		return str(self.feature_title)
 
 class Bar(models.Model):
 	bar_name = models.CharField(max_length = 100)
@@ -22,7 +24,9 @@ class Bar(models.Model):
 	manager = models.ForeignKey(User, on_delete = models.CASCADE)
 	approved = models.BooleanField(default = False)
 	region = models.ForeignKey(Region, on_delete = models.CASCADE)
-	features = models.ManyToManyField(Feature)
+	features = models.ManyToManyField(Feature, null=True, blank=True)
+	def __str__(self):
+		return str(self.bar_name) + ' ' + str(self.street_address)
 
 class HappyHour(models.Model):
 	day_of_week = models.CharField(max_length = 2)
@@ -32,9 +36,13 @@ class HappyHour(models.Model):
 	drinks = models.BooleanField(default = False)
 	food = models.BooleanField(default = False)
 	menu_pdf = models.CharField(max_length = 100)
+	def __str__(self):
+		return str(self.bar.bar_name) + ': ' + str(self.day_of_week)
 
 class Reviews(models.Model):
 	reviewer = models.ForeignKey(User, on_delete = models.CASCADE)
 	bar = models.ForeignKey(Bar, on_delete = models.CASCADE)
 	star_count = models.IntegerField()
+	def __str__(self):
+		return str(self.reviewer.username) + ': ' + str(self.bar) + ' - ' + str(self.star_count) + ' stars'
 
