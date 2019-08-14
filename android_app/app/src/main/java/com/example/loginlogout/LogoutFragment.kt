ee3df88777b51036c82203174f7c4f79ce7b9480
mlg1996 <32440812+mlg1996@users.noreply.github.com>
2019-08-14 11:52:04 -0500

    Added android app template

---
+++ android_app/app/src/main/java/com/example/loginlogout/LogoutFragment.kt
@@ -0,0 +1,26 @@
+package com.example.loginlogout
+
+import android.os.Bundle
+import android.text.Editable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.logout_fragment.*
+import kotlinx.android.synthetic.main.logout_fragment.view.*
+import org.json.JSONArray
+
+class LogoutFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.logout_fragment, container, false)
+        // Set an error if the password is less than 8 characters.
+        view.logout_button.setOnClickListener({
+            (activity as NavigationHost).navigateTo(LoginFragment(), false)
+        })
+        return view
+    }
+}
