f765eca31b704c240ea740a864cede3c4301a6d1
Brian <beadsdd@gmail.com>
2019-08-15 22:55:11 -0500

    Erased Venv

--- django_hh_project/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\beads\documents\msitm\summer\apad\happiest-hour\django_hh_project\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
