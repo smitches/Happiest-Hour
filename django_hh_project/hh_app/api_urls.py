cbf3038785e114ad85412287bc776ea8a668e318
Brian <beadsdd@gmail.com>
2019-08-16 13:34:08 -0500

    Token authentication and some API endpoints

--- django_hh_project/hh_app/api_urls.py
+++ django_hh_project/hh_app/api_urls.py
@@ -10,6 +10,18 @@ from rest_framework import routers
 
 urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
-    path('reviews/',ReviewsListView.as_view(),name='api_login'),
-    path('users/',UserListView.as_view(),name='api_users')
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+
+    path('reviews/',ReviewsListCreateView.as_view()),
+    path('users/',UserListView.as_view()),
+    path('bars/',BarListView.as_view()),
+    path('features/',FeatureListView.as_view()),
+    path('regions/',RegionListView.as_view()),
+    path('happyhours/',HappyHourListView.as_view()),
+
+    path('bar/<int:bar_id>/reviews/',BarReviewsView.as_view()),
+
+    path('userdetail/<int:pk>/',UserDetailView.as_view()),
+
+    path('auth_hello/',HelloView.as_view()),
 ]
