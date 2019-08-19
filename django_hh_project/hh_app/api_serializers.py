f05ad0eb455ab7a3438d5a1dab8088813a02a3e9
Brian <beadsdd@gmail.com>
2019-08-19 14:10:05 -0500

    Modified API endpoints

--- django_hh_project/hh_app/api_serializers.py
+++ django_hh_project/hh_app/api_serializers.py
@@ -2,43 +2,49 @@ from .models import *
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-class ReviewSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Reviews
-		fields = '__all__'
-		read_only_fields = ["reviewer"]
-
 class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
 		fields = ['id','username','first_name','last_name','email']
 
-class HappyHourSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = HappyHour
-		fields = '__all__'
-		read_only_fields = ["bar"]
-
 class BarSerializer(serializers.ModelSerializer):
+	manager = UserSerializer()
 	class Meta:
 		model = Bar
 		fields = '__all__'
 		read_only_fields = ["manager"]
+		depth = 2
+
+class ReviewSerializer(serializers.ModelSerializer):
+	reviewer = UserSerializer()
+	bar = BarSerializer()
+	class Meta:
+		model = Reviews
+		fields = '__all__'
+		read_only_fields = ["reviewer"]
+		depth = 2
+
+class HappyHourSerializer(serializers.ModelSerializer):
+	bar = BarSerializer()
+	class Meta:
+		model = HappyHour
+		fields = '__all__'
+		read_only_fields = ["bar"]
+		depth = 2
 
 class RegionSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Region
 		fields = '__all__'
+#		include_when_nested = ['region_name']
+
+#	def get_field_names(self, *args, **kwargs):
+#         field_names = super().get_field_names(*args, **kwargs)
+#         if self.parent:
+#             field_names = [i for i in field_names if i in self.Meta.include_when_nested]
+#         return field_names
 
 class FeatureSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Feature
 		fields = '__all__'
-
-# class HappyHourInfoSerializer(serializers.Serializer):
-# 	day = serializers.CharField(max_length=2,required=False)
-# 	region_id = serializers.IntegerField(required=False)
-# 	feature_ids = serializers.IntegerField(required=False)
-# 	star_count = serializers.FloatField(required=False)
-# 	drinks = serializers.BooleanField(required=False)
-# 	food = serializers.BooleanField(required=False)
