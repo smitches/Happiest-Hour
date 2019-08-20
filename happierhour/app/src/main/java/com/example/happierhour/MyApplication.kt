813d7a22176d4ae9b68f8be9b8a508f0d9dbde19
mlg1996 <32440812+mlg1996@users.noreply.github.com>
2019-08-19 21:05:32 -0500

    fixing merge conflicts

--- happierhour/app/src/main/java/com/example/happierhour/MyApplication.kt
+++ happierhour/app/src/main/java/com/example/happierhour/MyApplication.kt
@@ -8,12 +8,9 @@ class MyApplication : Application() {
         var filtered_hhs: String? = ""
         var bar_id: Int = 0
         var user = User_Model("","","","","")
-<<<<<<< HEAD
         var review_id: Int = 0
-=======
         var myBarIds = ArrayList<Int>()
         var hh_id: Int = 0
->>>>>>> 60ea6b7b58a3c14f077713eee6f44d84cfcbab31
     }
 
 }
